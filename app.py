21036ffe4b6d52fb2406f9c748bdaf577ee8d767
Milan Markovic <milan.markovic7572@gmail.com>
2024-07-20 09:59:26 +0200

    Baza projekta: login register dashboard logout css

--- app.py
+++ app.py
@@ -1,9 +1,87 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.secret_key = "your_secret_key"
+
+# Configure SQL Alchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+# Database Model
+class User(db.Model):
+    # Class Variables
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    if "username" in session:
+        return redirect(url_for("dashboard"))
+    return render_template("index.html")
+
+
+# Login
+@app.route('/login', methods=["POST"])
+def login():
+    # Collect info from the form
+    username = request.form['username']
+    password = request.form['password']
+
+    # Check if it's in the db / login
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        session['username'] = username
+        return redirect(url_for("dashboard"))
+    # If False show index
+    else:
+        return render_template("index.html")
+
+
+# Register
+@app.route("/register", methods=["POST"])
+def register():
+    # Collect info from the form
+    username = request.form['username']
+    password = request.form['password']
+
+    # Check if it's in the db / index
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return render_template("index.html", error="Already registered!")
+    else:
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        session["username"] = username
+        return redirect(url_for("dashboard"))
+
+
+# Dashboard
+@app.route("/dashboard")
+def dashboard():
+    if "username" in session:
+        return render_template("dashboard.html", username=session["username"])
+
+
+# Logout
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
