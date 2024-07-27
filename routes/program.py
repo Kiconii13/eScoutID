af383c00d46667787523bbff6f04156d5a4e15ee
Kiconii13 <markovic.milan7572@gmail.com>
2024-07-28 00:12:11 +0200

    Removed JS from program

--- routes/program.py
+++ routes/program.py
@@ -11,60 +11,48 @@ program_bp = Blueprint('program', __name__)
 @program_bp.route("/program")
 @login_required
 def program():
-    return render_template("program.html")
+    skills = Skill.query.filter_by(user_id = current_user.id).all()
+    return render_template("program.html", skills = skills)
 
 
 @program_bp.route('/addProgram', methods=['GET', 'POST'])
 @login_required
 def addProgram():
     users = User.query.filter_by(odred_id = current_user.odred.id)
+    selected_user = None
+    skills = []
+
     if request.method == 'POST':
-        user_id = request.form['user']
-        let_level = request.form['let_level']
-        zvezda_level = request.form['zvezda_level']
-        krin_level = request.form['krin_level']
+        if 'update_levels' in request.form:
+            user_id = request.form['user']
+            let_level = request.form['let_level']
+            zvezda_level = request.form['zvezda_level']
+            krin_level = request.form['krin_level']
 
-        user = User.query.get(user_id)
-        user.let_level = let_level
-        user.zvezda_level = zvezda_level
-        user.krin_level = krin_level
-        db.session.commit()
+            selected_user = User.query.get(user_id)
+            selected_user.let_level = let_level
+            selected_user.zvezda_level = zvezda_level
+            selected_user.krin_level = krin_level
+            db.session.commit()
+        elif 'select_user' in request.form:
+            user_id = request.form['user']
+            selected_user = User.query.get(user_id)
+            skills = Skill.query.filter_by(user_id = user_id).all()
+        elif 'add_skill' in request.form:
+            user_id = request.form['user_id']
+            name = request.form['name']
+            level = request.form['level']
+            date_got = datetime.strptime(request.form['date_got'], '%Y-%m-%d')
 
-        return redirect(url_for('program.addProgram'))
+            new_skill = Skill(user_id=user_id, name=name, level=level, date_got=date_got)
+            db.session.add(new_skill)
+            db.session.commit()
 
-    return render_template('addProgram.html', users=users)
+            selected_user = User.query.get(user_id)
+            skills = Skill.query.filter_by(user_id=user_id).all()
 
-@program_bp.route('/get_user_levels/<int:user_id>')
-@login_required
-def get_user_levels(user_id):
-    user = User.query.get(user_id)
-    return {
-        'let_level': user.let_level,
-        'zvezda_level': user.zvezda_level,
-        'krin_level': user.krin_level
-    }
+    if not selected_user and users:
+        selected_user = users[0]
+        skills = Skill.query.filter_by(user_id=selected_user.id).all()
 
-@program_bp.route('/get_user_skills/<int:user_id>')
-@login_required
-def get_user_skills(user_id):
-    skills = Skill.query.filter_by(user_id=user_id).all()
-    if skills:
-        skills_list = [{'name': skill.name, 'level': skill.level, 'date_got': skill.date_got.strftime('%Y-%m-%d')} for skill in skills]
-        return jsonify(skills_list)
-    else:
-        return jsonify([])
-
-@program_bp.route('/add_skill', methods=['POST'])
-@login_required
-def add_skill():
-    data = request.get_json()
-    user_id = data.get('user_id')
-    name = data.get('name')
-    level = data.get('level')
-    date_got = datetime.strptime(data.get('date_got'), '%Y-%m-%d')
-
-    new_skill = Skill(user_id=user_id, name=name, level=level, date_got=date_got)
-    db.session.add(new_skill)
-    db.session.commit()
-
-    return jsonify({'message': 'Skill added successfully'}), 201
+    return render_template('addProgram.html', users=users, selected_user=selected_user, skills=skills)
