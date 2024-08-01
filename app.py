7b298fcc8d99236df27c8e239df50c7eb12bbdd2
Despot Maksimovic <despotmaksimovic@gmail.com>
2024-08-01 17:25:54 +0200

    preparing for deployment

--- app.py
+++ app.py
@@ -3,28 +3,35 @@ from flask_login import LoginManager
 from models import db, User, Odred
 from routes import register_blueprints
 
-app = Flask(__name__)
-app.secret_key = "8PvUV36JVw59"
-
-# Configure SQL Alchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///eScoutID.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db.init_app(app)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
+from config import Config
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.filter_by(id=user_id).first()
+def create_app(config_class=Config):
+
+    app = Flask(__name__)
+
+    app.config.from_object(config_class)
+
+    # app.secret_key = "8PvUV36JVw59"
+
+    # Configure SQL Alchemy
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///eScoutID.db"
+    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
 
 
-# Register Blueprints
-register_blueprints(app)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.filter_by(id=user_id).first()
 
-if __name__ == '__main__':
+
+    # Register Blueprints
+    register_blueprints(app)
+
     with app.app_context():
         db.create_all()
 
@@ -46,5 +53,9 @@ if __name__ == '__main__':
             db.session.add(admin)
             db.session.commit()
 
+    return app
 
+
+if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
