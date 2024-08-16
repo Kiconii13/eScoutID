45294c808b4799b2e5accb004129f6909ee3b78a
Kiconii13 <milan.markovic7572@gmail.com>
2024-08-17 00:52:54 +0200

    minor dbudpate fix

--- db_update.py
+++ db_update.py
@@ -5,37 +5,17 @@ from sqlalchemy.exc import SQLAlchemyError
 def upgrade():
     try:
         with db.engine.connect() as connection:
-            # Proverite da li kolone postoje pre nego što ih dodate
-            result = connection.execute(text("""
-                SELECT column_name 
-                FROM information_schema.columns 
-                WHERE table_name = 'users'
-            """))
-            existing_columns = {row[0] for row in result}  # Koristite indeks 0 za pristup
-
-            if 'jmbg' not in existing_columns:
-                connection.execute(text('ALTER TABLE users ADD COLUMN jmbg VARCHAR(13);'))
-            if 'scout_id_number' not in existing_columns:
-                connection.execute(text('ALTER TABLE users ADD COLUMN scout_id_number VARCHAR(20);'))
-            if 'account_creation_date' not in existing_columns:
-                connection.execute(text('ALTER TABLE users ADD COLUMN account_creation_date TIMESTAMP;'))
-            if 'gender' not in existing_columns:
-                connection.execute(text('ALTER TABLE users ADD COLUMN gender VARCHAR(10);'))
-            if 'last_login_date' not in existing_columns:
-                connection.execute(text('ALTER TABLE users ADD COLUMN last_login_date TIMESTAMP;'))
+            # Dodajte kolone ako ne postoje
+            connection.execute(text('ALTER TABLE users ADD COLUMN IF NOT EXISTS jmbg VARCHAR(13);'))
+            connection.execute(text('ALTER TABLE users ADD COLUMN IF NOT EXISTS scout_id_number VARCHAR(20);'))
+            connection.execute(text('ALTER TABLE users ADD COLUMN IF NOT EXISTS account_creation_date TIMESTAMP;'))
+            connection.execute(text('ALTER TABLE users ADD COLUMN IF NOT EXISTS gender VARCHAR(10);'))
+            connection.execute(text('ALTER TABLE users ADD COLUMN IF NOT EXISTS last_login_date TIMESTAMP;'))
 
             # Promenite tip kolone ako je potrebno
             connection.execute(text('ALTER TABLE users ALTER COLUMN phone_number TYPE VARCHAR(13);'))
 
-        # Postavite polje 'gender' na osnovu 'jmbg' za sve postojeće korisnike
-        from models import User
-        users = User.query.all()
-        for user in users:
-            if user.jmbg and not user.gender:
-                user.calculate_gender()  # Pretpostavljam da imate ovu metodu u modelu User
-                db.session.commit()
-
-        print("Database successfully updated and genders set based on JMBG!")
+        print("Database successfully updated!")
 
     except SQLAlchemyError as e:
         print(f"An error occurred: {str(e)}")
