49c1772020d75cc8445440a7d9ae8e3c4605adff
Kiconii13 <milan.markovic7572@gmail.com>
2024-10-07 22:42:43 +0200

    Script for updating activity column

--- db_update.py
+++ db_update.py
@@ -6,14 +6,7 @@ def upgrade():
     try:
         with db.engine.connect() as connection:
             # Dodajte kolone ako ne postoje
-            connection.execute(text('ALTER TABLE users ADD COLUMN IF NOT EXISTS jmbg VARCHAR(13);'))
-            connection.execute(text('ALTER TABLE users ADD COLUMN IF NOT EXISTS scout_id_number VARCHAR(20);'))
-            connection.execute(text('ALTER TABLE users ADD COLUMN IF NOT EXISTS account_creation_date TIMESTAMP;'))
-            connection.execute(text('ALTER TABLE users ADD COLUMN IF NOT EXISTS gender VARCHAR(10);'))
-            connection.execute(text('ALTER TABLE users ADD COLUMN IF NOT EXISTS last_login_date TIMESTAMP;'))
-
-            # Promenite tip kolone ako je potrebno
-            connection.execute(text('ALTER TABLE users ALTER COLUMN phone_number TYPE VARCHAR(13);'))
+            connection.execute(text('ALTER TABLE activity ADD COLUMN IF NOT EXISTS key VARCHAR(50);'))
 
         print("Database successfully updated!")
 
