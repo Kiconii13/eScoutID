f7b8b562e49c00c6968b381148a6c2b5552f2799
Kiconii13 <milan.markovic7572@gmail.com>
2024-10-07 22:55:58 +0200

    Script for updating activity column

--- db_update.py
+++ db_update.py
@@ -2,16 +2,21 @@ from app import db, create_app
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 
+
 def upgrade():
     try:
-        with db.engine.connect() as connection:
-            # Dodajte kolone ako ne postoje
-            connection.execute(text('ALTER TABLE activities ADD COLUMN IF NOT EXISTS key VARCHAR(50);'))
+        # Pokrećemo novu transakciju
+        with db.session.begin():
+            # Dodajte kolonu ako ne postoji
+            db.session.execute(text('ALTER TABLE activities ADD COLUMN IF NOT EXISTS key VARCHAR(50);'))
 
+        db.session.commit()  # Obavezno sačuvaj promene
         print("Database successfully updated!")
 
     except SQLAlchemyError as e:
+        db.session.rollback()  # Ako dođe do greške, vrati sve promene unazad
         print(f"An error occurred: {str(e)}")
+
 
 if __name__ == "__main__":
     app = create_app()
