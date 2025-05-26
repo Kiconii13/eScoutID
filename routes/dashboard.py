828ad7abc34034e616964a98cf772d65c8f48bfe
Kiconii13 <milan.markovic7572@gmail.com>
2025-05-26 11:11:41 +0200

    Completed redesign for addAktivnost, addCetaVod, addClan, addProgram, aktivnosti, editRoles, editVod. Added personal and account.

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -11,42 +11,45 @@ dashboard_bp = Blueprint('dashboard', __name__)
 @dashboard_bp.route('/dashboard')
 @login_required
 def dashboard():
-    now = datetime.date.today()
-    return render_template("dashboard.html", now=now)
+    return render_template("dashboard.html")
 
+@dashboard_bp.route('/personal')
+@login_required
+def personal():
+    now = datetime.date.today()
+    return render_template("personal.html", now=now)
+
+@dashboard_bp.route('/account')
+@login_required
+def account():
+    return render_template("account.html")
 
 # Promena lozinke; Mora se uneti trenutna lozinka i nova lozinka sa potvrdom.
-@dashboard_bp.route('/dashboard/changePassword', methods=["POST", "GET"])
+@dashboard_bp.route('/dashboard/changePassword', methods=["POST"])
 @login_required
 def changePassword():
-    if request.method == "POST":
-        # Provera da li se unesena lozinka poklapa sa trenutnom lozinkom
-        if not current_user.check_password(request.form["current_password"]):
-            flash("Niste uneli tačnu trenutnu lozinku!", "Greška")
-            return redirect(url_for("dashboard.changePassword"))
-        # Uspešna promena
-        current_user.set_password(request.form["new_password"])
-        db.session.commit()
-        flash("Uspešno ste promenili lozinku!", "Info")
-        return redirect(url_for("dashboard.dashboard"))
-    else:
-        return render_template("changePassword.html")
+    # Provera da li se unesena lozinka poklapa sa trenutnom lozinkom
+    if not current_user.check_password(request.form["current_password"]):
+        flash("Niste uneli tačnu trenutnu lozinku!", "Greška")
+        return redirect(url_for("dashboard.changePassword"))
+    # Uspešna promena
+    current_user.set_password(request.form["new_password"])
+    db.session.commit()
+    flash("Uspešno ste promenili lozinku!", "Info")
+    return redirect(url_for("dashboard.dashboard"))
 
 
-@dashboard_bp.route('/dashboard/changeUsername', methods=["POST", "GET"])
+@dashboard_bp.route('/dashboard/changeUsername', methods=["POST"])
 @login_required
 def changeUsername():
-    if request.method == "POST":
-        if current_user.username != request.form["current_username"]:
-            flash("Netačno trenutno korisničko ime!", "Greška")
-            return redirect(url_for("dashboard.changeUsername"))
-        check_user = User.query.filter_by(username=request.form["new_username"]).first()
-        if check_user:
-            flash("Korisničko ime je već zauzeto", "Greška")
-            return redirect(url_for("dashboard.changeUsername"))
-        current_user.username = request.form["new_username"]
-        db.session.commit()
-        flash("Uspešno ste promenili korisničko ime!", "Info")
-        return redirect(url_for("dashboard.dashboard"))
-    else:
-        return render_template("changeUsername.html")
+    if current_user.username != request.form["current_username"]:
+        flash("Netačno trenutno korisničko ime!", "Greška")
+        return redirect(url_for("dashboard.changeUsername"))
+    check_user = User.query.filter_by(username=request.form["new_username"]).first()
+    if check_user:
+        flash("Korisničko ime je već zauzeto", "Greška")
+        return redirect(url_for("dashboard.changeUsername"))
+    current_user.username = request.form["new_username"]
+    db.session.commit()
+    flash("Uspešno ste promenili korisničko ime!", "Info")
+    return redirect(url_for("dashboard.dashboard"))
